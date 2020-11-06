0a4e08a35b3bd40d0c3405ce01f7d101d43d9919
Sinclert Pérez <Sinclert@hotmail.com>
2020-11-06 14:50:59 -0500

    Create models module init

---
+++ src/models/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from .base import BaseStaticModel
+from .base import BaseEvolvingModel
+
+from .category import Category
+from .membership import CategoryMembership
+
+from .jargon import Jargon
+from .jargon import JargonCategoryMetrics
+from .jargon import JargonPaperMetrics
+
+from .paper import Paper
+from .paper import PaperAuthor
+from .paper import PaperReferenceCounters
+from .reference import PaperReference
