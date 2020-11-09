9860fd0835c5b1f08a0c80da66899e2aa5ca183d
Sinclert Pérez <Sinclert@hotmail.com>
2020-11-09 15:22:29 -0500

    Add tests folder __init__

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,11 @@
-# File just to make the folder exists
+# This file is necessary to be able to allow imports from src
+
+import sys
+from pathlib import Path
+
+# Get the sources folder absolute path
+project_path = Path(__file__).parent.parent
+sources_path = project_path.joinpath("src")
+
+# Necessary for pytest to allow imports from packaged source folders
+sys.path.append(str(sources_path))
