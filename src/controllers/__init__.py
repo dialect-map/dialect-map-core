db72cc85565201eef7a40655f0a906772dee1156
Sinclert Pérez <Sinclert@hotmail.com>
2020-11-11 14:27:11 -0500

    Define Membership controller class

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -7,3 +7,5 @@ from .category_ctl import CategoryController
 from .jargon_ctl import JargonController
 from .jargon_ctl import JargonCategoryMetricsController
 from .jargon_ctl import JargonPaperMetricsController
+
+from .membership_ctl import MembershipController
