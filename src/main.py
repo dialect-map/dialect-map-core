d02aea380d8f4782c2893256f45c60c5aecd05b0
Sinclert Pérez <Sinclert@hotmail.com>
2020-11-23 13:27:48 -0500

    Create main launching script

---
+++ src/main.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+import atexit
+from flask import Flask
+from .config import ApplicationConfig
+from .config import EnvironmentConfigLoader
+from .logs import setup_logger
+from .service import AppService
+from .storage import JsonLoader
+from .storage import SQLAlchemyDatabase
+
+
+app = Flask(__name__)
+service: AppService
+
+
+def setup_service(c: ApplicationConfig) -> None:
+    """
+    Setup the global application service
+    :param c: global application configuration
+    """
+
+    global service
+
+    db_loader = JsonLoader([])
+    db_engine = SQLAlchemyDatabase(c.database_url, db_loader)
+    service = AppService(db_engine)
+
+    # Register the service cleanup function upon exiting
+    atexit.register(service.stop)
+
+
+# Gunicorn running the server
+if __name__ == "main":
+
+    loader = EnvironmentConfigLoader()
+    config = ApplicationConfig(loader)
+
+    # Setup order must be preserved
+    setup_logger(config.log_level)
+    setup_service(c=config)
+
+
+# Flask running the server
+if __name__ == "__main__":
+
+    loader = EnvironmentConfigLoader()
+    config = ApplicationConfig(loader)
+
+    # Setup order must be preserved
+    setup_logger(config.log_level)
+    setup_service(c=config)
+
+    # Run the application
+    app.run(host="0.0.0.0", port=8080, debug=False)
