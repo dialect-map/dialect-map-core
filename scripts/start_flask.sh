40d0a5ed09446c83eea86b2fd9149ca3af6f58df
Sinclert Pérez <Sinclert@hotmail.com>
2020-11-24 15:42:54 -0500

    Fix start-up Flask script

--- scripts/start_flask.sh
+++ scripts/start_flask.sh
@@ -5,9 +5,11 @@ set -o errexit
 set -o nounset
 
 # Define default values
+flask_app=${FLASK_APP:-"src/main:app"}
 flask_env=${FLASK_ENV:-"development"}
 
 # Set up environment variables
+export FLASK_APP=${flask_app}
 export FLASK_ENV=${flask_env}
 
 # Launches Flask
