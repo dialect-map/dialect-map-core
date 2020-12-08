1d93410d254b82d6115b3ae9bf694ce9dccf2b85
Sinclert Pérez <Sinclert@hotmail.com>
2020-12-07 21:30:28 -0500

    Define setup.py file

---
+++ setup.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+# Package meta-data
+NAME = "dialect-map-core"
+INFO = "Python package containing the core models of the Dialect Map"
+URL = "https://github.com/ds3-nyu-archive/dialect-map-core"
+REQUIRES_PYTHON = ">=3.6, <4"
+AUTHORS = "NYU DS3 Team"
+VERSION = open("VERSION", "r").read().strip()
+
+
+# Installation requirements
+INSTALLATION_REQS = [
+    "click==7.1.2",
+    "psycopg2-binary==2.8.6",
+    "sqlalchemy==1.3.20",
+]
+
+# Development requirements
+DEVELOPMENT_REQS = [
+    "black>=20.8b1",
+    "coverage>=5.0.4",
+    "mypy==0.790",
+    "pre-commit>=2.4.0",
+    "pytest>=6.0.2",
+    "pytest-cov>=2.10.0",
+]
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=INFO,
+    author=AUTHORS,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=INSTALLATION_REQS,
+    extras_require={
+        "dev": DEVELOPMENT_REQS,
+    },
+    include_package_data=True,
+    license="MIT",
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    cmdclass={},
+)
