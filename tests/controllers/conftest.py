86c81285cdb3d4956de76812f4e7d8fd48a19956
Sinclert Pérez <Sinclert@hotmail.com>
2021-01-27 15:33:00 -0500

    Define DB rollback function fixture

--- tests/controllers/conftest.py
+++ tests/controllers/conftest.py
@@ -2,16 +2,17 @@
 
 import pytest
 
+from src.dialect_map.controllers import BaseController
 from src.dialect_map.storage import JSONFileLoader
 from src.dialect_map.storage import SQLAlchemyDatabase
 from src.dialect_map_data import FILES_MAPPINGS
 
 
 @pytest.fixture(scope="package")
-def db() -> SQLAlchemyDatabase:
+def database() -> SQLAlchemyDatabase:
     """
     Creates a memory-based database to test database operations
-    :return: database object
+    :return: memory-based database object
     """
 
     loader = JSONFileLoader()
@@ -25,3 +26,18 @@ def db() -> SQLAlchemyDatabase:
         )
 
     return database
+
+
+@pytest.fixture(scope="function")
+def database_rollback(controller: BaseController):
+    """
+    Wraps a controller class test in order to rollback any DB operation.
+    It is equivalent to the combination of unit-test 'setUp' and 'tearDown' methods.
+    Its implementation is based on the following SQLAlchemy documentation
+    https://docs.sqlalchemy.org/en/13/orm/session_transaction.html (last section)
+    """
+
+    transaction = controller.db.conn.begin()
+    yield
+    controller.db.session.close()
+    transaction.rollback()
