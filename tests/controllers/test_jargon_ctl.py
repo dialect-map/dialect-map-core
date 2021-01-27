ddd297e08b08cb75b7c4356f713aea416f66e1b2
Sinclert Pérez <Sinclert@hotmail.com>
2021-01-27 16:02:44 -0500

    Group Jargon ctl tests together

--- tests/controllers/test_jargon_ctl.py
+++ tests/controllers/test_jargon_ctl.py
@@ -2,6 +2,7 @@
 
 import pytest
 from datetime import datetime
+
 from src.dialect_map.controllers import JargonController
 from src.dialect_map.controllers import JargonGroupController
 from src.dialect_map.models import Jargon
@@ -9,156 +10,164 @@ from src.dialect_map.models import JargonGroup
 from src.dialect_map.storage import BaseDatabase
 
 
-def test_jargon_get(db: BaseDatabase):
-    """
-    Tests the retrieval of a jargon using a controller
-    :param db: initiated and loaded database
-    """
+@pytest.mark.usefixtures("database_rollback")
+class TestJargonController:
+    """ Class to group all the Jargon model controller tests """
 
-    controller = JargonController(db=db)
-    jargon = controller.get("jargon-01234")
+    @pytest.fixture(scope="class")
+    def controller(self, database: BaseDatabase):
+        """
+        Creates a memory-based controller for the Jargon records
+        :param database: dummy database instance
+        :return: initiated controller instance
+        """
 
-    assert type(jargon) is Jargon
-    assert jargon.id == "jargon-01234"
+        return JargonController(db=database)
 
+    def test_get(self, controller: JargonController):
+        """
+        Tests the retrieval of a jargon by the controller
+        :param controller: initiated instance
+        """
 
-def test_jargon_get_by_string(db: BaseDatabase):
-    """
-    Tests the retrieval of a jargon using a controller
-    :param db: initiated and loaded database
-    """
+        jargon_id = "jargon-01234"
+        jargon_obj = controller.get(jargon_id)
 
-    controller = JargonController(db=db)
-    jargon = controller.get_by_string("One string")
+        assert type(jargon_obj) is Jargon
+        assert jargon_obj.id == jargon_id
 
-    assert type(jargon) is Jargon
-    assert jargon.id == "jargon-01234"
+    def test_get_by_string(self, controller: JargonController):
+        """
+        Tests the retrieval of a jargon by the controller
+        :param controller: initiated instance
+        """
 
+        jargon = controller.get_by_string("One string")
 
-def test_jargon_get_by_group(db: BaseDatabase):
-    """
-    Tests the retrieval of jargon groups using a controller
-    :param db: initiated and loaded database
-    """
+        assert type(jargon) is Jargon
+        assert jargon.id == "jargon-01234"
 
-    controller = JargonController(db=db)
-    groups = controller.get_by_group()
+    def test_get_by_group(self, controller: JargonController):
+        """
+        Tests the retrieval of a jargon by the controller
+        :param controller: initiated instance
+        """
 
-    assert type(groups) is list
-    assert len(groups) == 1
-    assert len(groups[0]) == 2
+        groups = controller.get_by_group()
 
+        assert type(groups) is list
+        assert len(groups) == 1
+        assert len(groups[0]) == 2
 
-def test_jargon_create(db: BaseDatabase):
-    """
-    Tests the creation of a jargon using a controller
-    :param db: initiated and loaded database
-    """
+    def test_create(self, controller: JargonController):
+        """
+        Tests the creation of a jargon by the controller
+        :param controller: initiated instance
+        """
 
-    controller = JargonController(db=db)
+        jargon_id = "jargon-creation"
+        jargon_ds = "My test jargon"
+        jargon_re = "[Mm]y test jargon"
+        jargon = Jargon(
+            jargon_id=jargon_id,
+            jargon_str=jargon_ds,
+            jargon_regex=jargon_re,
+            num_words=len(jargon_ds.split(" ")),
+            created_at=datetime.now(),
+        )
 
-    jargon_id = "jargon-creation"
-    jargon_ds = "My test jargon"
-    jargon_re = "[Mm]y test jargon"
-    jargon = Jargon(
-        jargon_id=jargon_id,
-        jargon_str=jargon_ds,
-        jargon_regex=jargon_re,
-        num_words=len(jargon_ds.split(" ")),
-        created_at=datetime.now(),
-    )
+        creation_id = controller.create(jargon)
+        created_obj = controller.get(jargon_id)
 
-    creation_id = controller.create(jargon)
-    created_obj = controller.get(jargon_id)
+        assert creation_id == jargon_id
+        assert created_obj == jargon
 
-    assert creation_id == jargon_id
-    assert created_obj == jargon
+    def test_delete(self, controller: JargonController):
+        """
+        Tests the deletion of a jargon by the controller
+        :param controller: initiated instance
+        """
 
+        jargon_id = "jargon-deletion"
+        jargon_ds = "My test jargon"
+        jargon_re = "[Mm]y test jargon"
+        jargon = Jargon(
+            jargon_id=jargon_id,
+            jargon_str=jargon_ds,
+            jargon_regex=jargon_re,
+            num_words=len(jargon_ds.split(" ")),
+            created_at=datetime.now(),
+        )
 
-def test_jargon_delete(db: BaseDatabase):
-    """
-    Tests the deletion of a jargon using a controller
-    :param db: initiated and loaded database
-    """
+        creation_id = controller.create(jargon)
+        deletion_id = controller.delete(jargon_id)
 
-    controller = JargonController(db=db)
-
-    jargon_id = "jargon-deletion"
-    jargon_ds = "My test jargon"
-    jargon_re = "[Mm]y test jargon"
-    jargon = Jargon(
-        jargon_id=jargon_id,
-        jargon_str=jargon_ds,
-        jargon_regex=jargon_re,
-        num_words=len(jargon_ds.split(" ")),
-        created_at=datetime.now(),
-    )
-
-    creation_id = controller.create(jargon)
-    deletion_id = controller.delete(jargon_id)
-
-    assert creation_id == deletion_id
-
-    with pytest.raises(ValueError):
-        controller.get(jargon_id)
-
-
-def test_jargon_group_get(db: BaseDatabase):
-    """
-    Tests the retrieval of a jargon group using a controller
-    :param db: initiated and loaded database
-    """
-
-    controller = JargonGroupController(db=db)
-    group = controller.get("jargon-group-01234")
-
-    assert type(group) is JargonGroup
-    assert group.id == "jargon-group-01234"
+        assert creation_id == deletion_id
+        assert pytest.raises(ValueError, controller.get, jargon_id)
 
 
-def test_jargon_group_create(db: BaseDatabase):
-    """
-    Tests the creation of a jargon group using a controller
-    :param db: initiated and loaded database
-    """
+@pytest.mark.usefixtures("database_rollback")
+class TestJargonGroupController:
+    """ Class to group all the JargonGroup model controller tests """
 
-    controller = JargonGroupController(db=db)
+    @pytest.fixture(scope="class")
+    def controller(self, database: BaseDatabase):
+        """
+        Creates a memory-based controller for the JargonGroup records
+        :param database: dummy database instance
+        :return: initiated controller instance
+        """
 
-    group_id = "jargon-group-creation"
-    group_ds = "My test group"
-    group = JargonGroup(
-        group_id=group_id,
-        description=group_ds,
-        created_at=datetime.now(),
-    )
+        return JargonGroupController(db=database)
 
-    creation_id = controller.create(group)
-    created_obj = controller.get(group_id)
+    def test_get(self, controller: JargonGroupController):
+        """
+        Tests the retrieval of a jargon group by the controller
+        :param controller: initiated instance
+        """
 
-    assert creation_id == group_id
-    assert created_obj == group
+        group_id = "jargon-group-01234"
+        group_obj = controller.get(group_id)
 
+        assert type(group_obj) is JargonGroup
+        assert group_obj.id == group_id
 
-def test_jargon_group_delete(db: BaseDatabase):
-    """
-    Tests the deletion of a jargon group using a controller
-    :param db: initiated and loaded database
-    """
+    def test_create(self, controller: JargonGroupController):
+        """
+        Tests the creation of a jargon group by the controller
+        :param controller: initiated instance
+        """
 
-    controller = JargonGroupController(db=db)
+        group_id = "jargon-group-creation"
+        group_ds = "My test group"
+        group = JargonGroup(
+            group_id=group_id,
+            description=group_ds,
+            created_at=datetime.now(),
+        )
 
-    group_id = "jargon-group-deletion"
-    group_ds = "My test group"
-    group = JargonGroup(
-        group_id=group_id,
-        description=group_ds,
-        created_at=datetime.now(),
-    )
+        creation_id = controller.create(group)
+        created_obj = controller.get(group_id)
 
-    creation_id = controller.create(group)
-    deletion_id = controller.delete(group_id)
+        assert creation_id == group_id
+        assert created_obj == group
 
-    assert creation_id == deletion_id
+    def test_delete(self, controller: JargonGroupController):
+        """
+        Tests the deletion of a jargon group by the controller
+        :param controller: initiated instance
+        """
 
-    with pytest.raises(ValueError):
-        controller.get(group_id)
+        group_id = "jargon-group-deletion"
+        group_ds = "My test group"
+        group = JargonGroup(
+            group_id=group_id,
+            description=group_ds,
+            created_at=datetime.now(),
+        )
+
+        creation_id = controller.create(group)
+        deletion_id = controller.delete(group_id)
+
+        assert creation_id == deletion_id
+        assert pytest.raises(ValueError, controller.get, group_id)
