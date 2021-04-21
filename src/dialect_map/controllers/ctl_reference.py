042064a4f615b636cf6bc888655102f4a3105563
Sinclert Pérez <Sinclert@hotmail.com>
2021-04-21 17:32:11 -0400

    Improve controllers doc-strings

--- src/dialect_map/controllers/ctl_reference.py
+++ src/dialect_map/controllers/ctl_reference.py
@@ -14,10 +14,10 @@ class ReferenceController(StaticController[PaperReference]):
 
     def get_by_source_paper(self, arxiv_id: str, arxiv_rev: int) -> list:
         """
-        Gets a database record by its ID
+        Gets a list of paper references given the source
         :param arxiv_id: ID of the reference source paper
         :param arxiv_rev: revision of the reference source paper
-        :return: list of data object representing database records
+        :return: list of database objects
         """
 
         query = self.db.session.query(self.model)
@@ -28,10 +28,10 @@ class ReferenceController(StaticController[PaperReference]):
 
     def get_by_target_paper(self, arxiv_id: str, arxiv_rev: int) -> list:
         """
-        Gets a database record by its ID
+        Gets a list of paper references given the target
         :param arxiv_id: ID of the reference target paper
         :param arxiv_rev: revision of the reference target paper
-        :return: list of data object representing database records
+        :return: list of database objects
         """
 
         query = self.db.session.query(self.model)
