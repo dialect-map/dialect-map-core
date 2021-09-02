f6008246ccfe8e30abb05741e1a606330790e476
Sinclert Pérez <Sinclert@hotmail.com>
2021-09-02 14:06:36 +0200

    Define CLI command tests

---
+++ tests/test_cli.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+from click.testing import CliRunner
+
+from src.dialect_map.cli import main
+
+
+@pytest.fixture(scope="module")
+def env() -> dict:
+    """
+    Defines the set of environment variables available to the CLI runner
+    :return: dict of environment variables
+    """
+
+    return {
+        "DIALECT_MAP_DB_URL": "sqlite:///:memory:",
+    }
+
+
+def test_cli_load_db(env: dict):
+    """
+    Tests the invocation of the DB loading CLI command
+    :param env: dictionary of environment variables
+    """
+
+    runner = CliRunner(env=env)
+    result = runner.invoke(main, "load-db")
+
+    assert result.exit_code == 0
+    assert result.output == ""
+
+
+def test_cli_setup_db(env: dict):
+    """
+    Tests the invocation of the DB set-up CLI command
+    :param env: dictionary of environment variables
+    """
+
+    runner = CliRunner(env=env)
+    result = runner.invoke(main, "setup-db")
+
+    assert result.exit_code == 0
+    assert result.output == ""
+
+
+def test_cli_teardown_db(env: dict):
+    """
+    Tests the invocation of the DB tear-down CLI command
+    :param env: dictionary of environment variables
+    """
+
+    runner = CliRunner(env=env)
+    result = runner.invoke(main, "teardown-db")
+
+    assert result.exit_code == 0
+    assert result.output == ""
+
+    result = runner.invoke(main, "teardown-db --force")
+
+    assert result.exit_code == 1
+    assert result.output != ""
