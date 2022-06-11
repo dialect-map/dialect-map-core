2971da856a88c35abf8d5aebe79ad68d6cb62d5a
Sinclert Pérez <sinclert@hotmail.com>
2022-06-11 15:43:23 +0200

    Activate SQLite foreign key integrity check

--- tests/storage/conftest.py
+++ tests/storage/conftest.py
@@ -15,4 +15,9 @@ def database() -> SQLAlchemyDatabase:
     database = SQLAlchemyDatabase("sqlite:///:memory:")
     database.setup(check=False)
 
+    ### NOTE:
+    ### SQLite does not check foreign key integrity by default
+    ### Ref: https://docs.sqlalchemy.org/en/14/dialects/sqlite.html#foreign-key-support
+    database.session.execute("PRAGMA foreign_keys=ON")
+
     return database
